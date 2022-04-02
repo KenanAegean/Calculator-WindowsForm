fa366cbfb658c1086d512b8b5540edf52e58062d
Kenan Ege <61656752+KenanAegean@users.noreply.github.com>
2022-04-02 19:47:58 +0200

    last version

--- CalculatorAppForm3/Form1.cs
+++ CalculatorAppForm3/Form1.cs
@@ -15,12 +15,113 @@ namespace CalculatorAppForm3
         private float result;
         private string op = "";
         private bool isEvaluate = true;
+
+        //Button[] numButtons;
+        private Button[] numButtons = new Button[10];
+        private Button[] opButtons = new Button[6];
+
+
         public Form1()
         {
+            dynamicNumButtonMaker();
+            dynamicOpButtonMaker();
+
             InitializeComponent();
             calculationShow.Text = "";
         }
-        
+
+        private void dynamicOpButtonMaker()
+        {
+            String[] operators = { "/", "*", "-", "+", "=" };
+            int y = 0;
+            int x = 0;
+            for (int i = 0; i < opButtons.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    opButtons[0] = new Button
+                    {
+                        Name = "btnClear",
+                        Location = new Point(178, 85),
+                        Size = new Size(50, 50),
+                        BackColor = Color.DarkOrange,
+                        ForeColor = Color.White,
+                        Text = "C"
+                    };
+                    opButtons[0].Click += new EventHandler(this.btnClear_Click);
+
+                }
+                else
+                {
+                    opButtons[i] = new Button
+                    {
+                        Name = "opButton" + operators[i - 1],
+                        Location = new Point(178 - 55 * y, 85 + 55 * (x + 1)),
+                        Size = new Size(50, 50),
+                        BackColor = Color.Black,
+                        ForeColor = Color.White,
+                        Text = operators[i - 1]
+                    };
+
+                    if (i >= 3)
+                    {
+                        y++;
+                        x = 2;
+                    }
+                    else
+                    {
+                        x++;
+                    }
+                    opButtons[i].Click += new EventHandler(this.operatorsClicked);
+                }
+                this.Controls.Add(opButtons[i]);
+
+
+            }
+
+
+
+
+        }
+
+       
+
+        private void dynamicNumButtonMaker()
+        {
+            int line = 0;
+            int column = 0;
+
+            for (int i = 0; i < numButtons.Length; i++)
+            {
+                numButtons[i] = new Button
+                {
+                    Location = new Point(13 + column * 55, 250 - line * 55),
+                    Name = "button" + i,
+                    Size = new Size(50, 50),
+                    TabIndex = i + 1,
+                    Text = (i).ToString(),
+                    //UseVisualStyleBackColor = true,
+                    BackColor = Color.DarkSlateGray,
+                    ForeColor = Color.White
+                };
+                numButtons[i].Click += new EventHandler(this.numsClicked);
+                column = ++column % 3;
+
+                if (i == 0)
+                {
+                    column = 0;
+                }
+                this.Controls.Add(numButtons[i]);
+
+                if (column == 0)
+                {
+                    line++;
+                }
+
+            }
+        }
+
         private void numsClicked(object sender, EventArgs e)
         {
             if (isEvaluate)
@@ -33,6 +134,7 @@ namespace CalculatorAppForm3
         }
         private void operatorsClicked(object sender, EventArgs e)
         {
+            //some kinde error here solve!!
             isEvaluate = true;
             float number = Convert.ToUInt32(txtResult.Text);
             switch (op)
@@ -54,10 +156,11 @@ namespace CalculatorAppForm3
                     break;
                 default:
                     result = number;
+                    txtResult.Clear();
                     break;
             }
             op = (sender as Button).Text;
-            txtResult.Clear();
+            //txtResult.Clear();
             
             //checking section for correct position of =
             if ((sender as Button).Text == "=")
@@ -81,6 +184,8 @@ namespace CalculatorAppForm3
             isEvaluate = true;
             op = "";
         }
+
+        
     }
 }
 
